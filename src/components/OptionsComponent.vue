3b46106d2124535892674aee8875e8b0f3c2f3b0
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-21 17:07:14 +0700

    feat: add composition and options component

---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'Razka Agniatara',
+        }
+    },
+
+    methods: {
+        showName() {
+            console.log(this.name)
+        },
+    },
+}
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+    <button @click="showName">Show Name</button>
+</template>
