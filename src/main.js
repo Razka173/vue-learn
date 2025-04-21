1e7fce10ca12370b8b0f1eea255f25dd8c7c0dc2
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-21 18:37:49 +0700

    feat: learn global registration component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import CompositionComponent from './components/CompositionComponent.vue'
+import OptionsComponent from './components/OptionsComponent.vue'
+import MainButton from './components/MainButton.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('CompositionComponent', CompositionComponent)
+app.component('OptionsComponent', OptionsComponent)
+app.component('MainButton', MainButton)
+
+app.mount('#app')
