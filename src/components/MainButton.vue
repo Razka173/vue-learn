72fecfbf500a6c9962cb7872a59b9a483046416e
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-22 13:59:14 +0700

    feat: learn mustache as function

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,14 +1,22 @@
 <script setup>
-const isLoading = true
+const isLoading = false
 const buttonAttr = {
     disabled: isLoading,
     class: 'text-3xl bg-black text-white',
+}
+
+const renderIsLoading = () => {
+    if (isLoading) {
+        return 'Loading'
+    } else {
+        return 'Submit'
+    }
 }
 </script>
 
 <template>
     <button v-bind="buttonAttr" :id="`id-${buttonAttr.class}`">
-        {{ isLoading ? 'Loading' : 'Submit' }}
+        {{ renderIsLoading() }}
     </button>
 </template>
 
