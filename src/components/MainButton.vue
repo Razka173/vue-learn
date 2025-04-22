52b9ce7dbfb2af176c7445a4b431cf7b6a9778a8
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-22 13:53:16 +0700

    feat: learn javascript syntax

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,12 +1,13 @@
 <script setup>
+const isLoading = true
 const buttonAttr = {
-    disabled: true,
+    disabled: isLoading,
     class: 'text-3xl bg-black text-white',
 }
 </script>
 
 <template>
-    <button v-bind="buttonAttr">Razka Agniatara</button>
+    <button v-bind="buttonAttr">{{ isLoading ? 'Loading' : 'Submit' }}</button>
 </template>
 
 <style scoped></style>
