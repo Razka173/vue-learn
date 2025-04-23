4a6bd524bad4c52c9a35b6220103df0cb7945f87
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-23 16:02:01 +0700

    feat: learn list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { computed, reactive, ref } from 'vue'
+import MainButton from './components/MainButton.vue'
 
 const showAuthor = ref(false)
 
 const author = reactive({
     name: 'JK Rowling',
-    books: ['Harry Potter 1', 'Harry Potter 2'],
+    books: [
+        {
+            title: 'Harry Potter 1',
+            releasedDate: '1997',
+            reviews: [5, 5, 5],
+            avgReview: 5,
+        },
+        {
+            title: 'Harry Potter 2',
+            releasedDate: '1999',
+            reviews: [1, 2, 3],
+            avgReview: 2,
+        },
+    ],
 })
 
 const count = ref(0)
@@ -35,6 +49,32 @@ const addBook = () => {
         <span v-else-if="author.books.length === 1">Have a Book</span>
         <span v-else>Don't have a Book</span>
     </h1>
+    <h2>Books:</h2>
+    <ul>
+        <li
+            v-for="({ title, releasedDate, reviews }, index) in author.books"
+            :key="`book-${index}`"
+        >
+            {{ title }} - {{ releasedDate }}
+            <span v-for="(review, reviewIndex) in reviews" :key="`review-${reviewIndex}`">
+                {{ review }}
+            </span>
+        </li>
+    </ul>
+
+    <ul>
+        <li v-for="(data, key, index) in author" :key="key">{{ index }}. {{ key }}, {{ data }}</li>
+    </ul>
+
+    <ol>
+        <li v-for="(item, index) in 10" :key="index">Razka Agniatara</li>
+    </ol>
+
+    <ul>
+        <template v-for="(item, index) in author.books" :key="index">
+            <MainButton :title="item.title" />
+        </template>
+    </ul>
 </template>
 
 <style scoped></style>
