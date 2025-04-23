bf0fe871b442db92d3551c69c4758c436fa79517
Razka Agniatara <razka.agniatara@balitower.co.id>
2025-04-23 13:28:24 +0700

    feat: learn v-if directive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed, reactive, ref } from 'vue'
 
+const showAuthor = ref(false)
+
 const author = reactive({
     name: 'JK Rowling',
     books: ['Harry Potter 1', 'Harry Potter 2'],
@@ -13,16 +15,26 @@ const increment = () => {
 }
 
 const isPublished = computed(() => {
-    console.log('test')
+    console.log(author.books)
     return author.books.length > 0 ? 'Published yes' : 'Publised no'
 })
+
+const addBook = () => {
+    author.books.push('Harry Potter 3')
+}
 </script>
 
 <template>
     <div>
         {{ isPublished }}
     </div>
-    <button @click="increment">{{ count }}</button>
+    <button @click="addBook">{{ count }}</button>
+    <h1>
+        {{ author.name }}
+        <span v-if="author.books.length > 1">Have more than one Book</span>
+        <span v-else-if="author.books.length === 1">Have a Book</span>
+        <span v-else>Don't have a Book</span>
+    </h1>
 </template>
 
 <style scoped></style>
