53b938b9347b40e083a3a4ba66fb7aa699b561c7
Razka Agniatara <razka173@gmail.com>
2025-04-24 09:07:51 +0700

    feat: learn event handling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref } from 'vue';
 import MainButton from './components/MainButton.vue'
-const disabled = ref(false);
-const toggleDisable = () => {
-    disabled.value = !disabled.value
+
+const count = ref(0);
+
+const increment = (counter) => {
+    count.value = count.value + counter
 }
+
+
 </script>
 
 <template>
-    <MainButton @click="toggleDisable" :disabled></MainButton>
+    <button  @click="(event) => console.log(event)" @mouseover="increment(10)">{{ count }}</button>
 </template>
 
 <style scoped></style>
